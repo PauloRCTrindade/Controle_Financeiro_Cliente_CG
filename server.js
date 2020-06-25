029b5eff16b424e505d01887784fe7edd2b8951e
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-25 18:35:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require('./routes')
+const methodOverride = require('method-override')
 
 
 const server = express()
 
+server.use(express.urlencoded({extended: true}))
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(routes)
 
 server.set("view engine","njk")
-nunjucks.configure("views",{
-  express: server
-})
 
-server.get('/',function(req,res){
-    return res.render('create')
+nunjucks.configure("views",{
+  express: server,
+  autoescape: false,
+  noCache: true
 })
 
 server.listen(3333,function(){
