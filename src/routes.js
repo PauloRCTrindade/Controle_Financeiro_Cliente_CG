000560fe77ec110c4ed9060593a6467921bdf798
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-30 19:41:28 -0300

    reorganização

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const routes  = express.Router()
+const membro = require('./app/controllers/membros')
+
+
+routes.get('/',function(req,res){
+  return res.redirect('/membros')
+})
+
+routes.get('/create',membro.post)
+routes.put('/create',membro.put)
+routes.post('/create',membro.create)
+routes.delete('/create',membro.delete)
+
+routes.get('/membros/:id',membro.show)
+routes.get('/membros/:id/edit',membro.edit)
+
+routes.get('/membros',membro.membros)
+routes.get('/home',membro.home)
+
+
+
+
+
+
+
+
+
+module.exports = routes
