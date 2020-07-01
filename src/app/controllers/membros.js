4034a0ed1e729a4c2b76eb6a1fbb5f9cd5fdabda
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-01 18:48:08 -0300

    Update membros.js

--- src/app/controllers/membros.js
+++ src/app/controllers/membros.js
@@ -1,143 +1,93 @@
-const fs    = require('fs')
-const data  = require('./data.json')
-const {age,date} = require('./utils')
+const {age,date} = require('../../lib/utils')
+const db = require('../../config/db')
+const membro = require('../models/membro')
 
-exports.membros = function(req,res){
 
-  return res.render('membros',{membros: data.membros})
-}
-//Create
-exports.create = function(req,res){
-  const keys   = Object.keys(req.body)
-  const foto = 'https://images.unsplash.com/photo-1528709024086-98a7672e0b9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60/500x500'
-  
-  if(req.body.link_foto == ""){
-    req.body.link_foto = foto
-  }
 
-  for(item of keys){
-    if(req.body[item] == ""){
-      return res.send('Somente o campo "Link para a foto" pode ficar vazio')
+module.exports = {
+  index(req,res){
+    
+
+
+    
+  },
+  membros(req,res){
+    
+      membro.all(function(membros){
+        return res.render('membros',{ membros})
+      })
+  },
+  create(req,res){
+    return res.render('create')
+  },
+  post(req,res){
+    const keys   = Object.keys(req.body)
+    const foto = 'https://images.unsplash.com/photo-1528709024086-98a7672e0b9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60/500x500'
+    
+    if(req.body.link_foto == ""){
+      req.body.link_foto = foto
     }
-  }
-
-  let {nome, idade, genero,link_foto,} = req.body
-
-  idade = Date.parse(idade)
-  const desde = Date.now()
-  const id    = Number(data.membros.length + 1)
-  const valores = []
   
-  data.membros.push({
-    id,
-    nome,
-    idade,
-    genero,
-    link_foto,
-    desde,
-    valores
-  })
-  
-  
-  fs.writeFile("data.json",JSON.stringify(data,null,2),function(err){
-    if(err) return res.send('Erro ao gravar os dados')
-      return res.redirect('/create')
-  })
-  
-}
-
-exports.post = function(req,res){
-  return res.render('create')
-}
-
-exports.home = function(req,res){
-  return res.render('index')
-}
-
-exports.show = function(req,res){
-  const {id} = req.params
-
-
-  const foundMembros = data.membros.find(function(membro){
-    return membro.id == id
-  })
-
-  if (!foundMembros) return res.send('Membro não encontrado')
-
-  const membro = {
-    ...foundMembros,
-    idade: age(foundMembros.idade),
-    genero:"",
-    desde: new Intl.DateTimeFormat("pt-BR").format(foundMembros.desde),
-  }
-
-  return res.render('show',{membro})
-
-}
-
-exports.edit = function(req,res){
-  const {id} = req.params
-
-
-  const foundMembros = data.membros.find(function(membros){
-    return membros.id == id
-  })
-
-  if (!foundMembros) return res.send('Membro não encontrado')
-
-  const membro = {
-    ...foundMembros,
-    idade: date(foundMembros.idade)
-  }
-
-
-
-  return res.render("edit",{ membro })
-}
-
-exports.put = function(req,res){
-  const {id} = req.body
-  let index = 0
-
-  const foundMembros = data.membros.find(function(membros,foundIndex){
-    if( id == membros.id){
-      index = foundIndex
-      return true
+    for(item of keys){
+      if(req.body[item] == ""){
+        return res.send('Somente o campo "Link para a foto" pode ficar vazio')
+      }
     }
-  })
+    const query = `
+      INSERT INTO membros(
+        nome,
+        link_foto,
+        genero,
+        idade,
+        desde
+      )VALUES ($1, $2, $3, $4, $5)
+      RETURNING id
+      
+    `
+    const values = [
+      req.body.nome,
+      req.body.link_foto,
+      req.body.genero,
+      date(req.body.idade).iso,
+      date(Date.now()).iso
+    ]
 
-  if (!foundMembros) return res.send('Membro não encontrado')
+    db.query(query,values,function(err,results){
+      if(err) return res.send("Erro na base de dados")
+  
+      return res.redirect(`membros/${results.rows[0].id}`)
+    })
 
-  const membro = {
-    ...foundMembros,
-    ...req.body,
-    idade: Date.parse(req.body.idade),
-    id: Number(req.body.id)
-  }
 
-  data.membros[index] = membro
+  },
 
-  fs.writeFile("data.json", JSON.stringify(data,null,2),function(err){
-    if (err) return res.send("Dados não foram alterados")
+  home(req,res){
+    return res.render('index')
+  },
 
-    return res.redirect(`/membros/${id}`)
-  })
-
+  show(req,res){
+    return res.render('index')
+  },
+  edit(req,res){
+    const keys   = Object.keys(req.body)
+    const foto = 'https://images.unsplash.com/photo-1528709024086-98a7672e0b9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60/500x500'
+    
+    if(req.body.link_foto == ""){
+      req.body.link_foto = foto
+    }
+  
+    for(item of keys){
+      if(req.body[item] == ""){
+        return res.send('Somente o campo "Link para a foto" pode ficar vazio')
+      }
+    }
+    return
+  },
+  delete(req,res){
+    return
+  },
 }
 
-exports.delete = function(req,res){
-  const {id} = req.body
 
-  const filtroMembros = data.membros.filter(function(membros){
-    return membros.id != id
-  })
-
-  data.membros = filtroMembros
-
-  fs.writeFile('data.json',JSON.stringify(data,null,2),function(err){
-    if(err) return res.send('O Arquivo não foi deleteado')
-    return res.redirect('/membros')
-  })
-}
 
 
