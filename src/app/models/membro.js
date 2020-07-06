1af734a100da3e156c4be036f2ed40cec9435977
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-06 17:46:22 -0300

    Update membro.js

--- src/app/models/membro.js
+++ src/app/models/membro.js
@@ -4,7 +4,7 @@ const { date } = require('../../lib/utils')
 module.exports = {
   all(callback){
     db.query(`SELECT * FROM membros`,function(err,results){
-      if(err) return res.send("Erro na base de dados")
+      if(err) throw `Erro na base de dados ${err}`
 
       callback(results.rows)
     })
@@ -31,7 +31,7 @@ module.exports = {
     ]
 
     db.query(query,values,function(err,results){
-      if(err) return res.send("Erro na base de dados")
+      if(err) throw "Erro na base de dados"
   
       callback(results.rows[0])
     })
@@ -39,10 +39,37 @@ module.exports = {
   },
   find(id,callback) {
     db.query(`SELECT * 
-    FROM  membros 
-    WHERE id = $1 `, [id],function(err,results){
-      if (err) return res.send('ERRO no Banco de Dados')
-      callback(results.rows[0])
+      FROM  membros 
+      WHERE id = $1 `, [id],function(err,results){
+        if(err) return "ERRO"//throw `${err}`
+
+         callback(results.rows[0])
     })
+  },
+
+  update(data,callback){
+    const query = `UPDATE membros SET
+    link_foto=($1),
+    nome=($2),
+    genero=($3),
+    idade=($4)
+    WHERE id = $5
+    `
+    const values = [
+      data.link_foto,
+      data.nome,
+      data.genero,
+      date(data.idade).iso,
+      data.id
+    ]
+
+    db.query(query,values,function(err,results){
+      if (err) throw `ERRO no Banco de Dados ${err}`
+        callback()
+
+    })
+      
+    
+
   }
 }
