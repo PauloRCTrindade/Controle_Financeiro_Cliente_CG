aabcf36396d534a70ae865cc0e23220f763ea30d
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-06 12:22:19 -0300

    Update membro.js

--- src/app/models/membro.js
+++ src/app/models/membro.js
@@ -1,3 +1,6 @@
+const db = require('../../config/db')
+const { date } = require('../../lib/utils')
+
 module.exports = {
   all(callback){
     db.query(`SELECT * FROM membros`,function(err,results){
@@ -7,7 +10,39 @@ module.exports = {
     })
 
   },
-  create(){
+  create(data,callback){
+    const query = `
+      INSERT INTO membros(
+        nome,
+        link_foto,
+        genero,
+        idade,
+        desde
+      )VALUES ($1, $2, $3, $4, $5)
+      RETURNING id
+      
+    `
+    const values = [
+      data.nome,
+      data.link_foto,
+      data.genero,
+      date(data.idade).iso,
+      date(Date.now()).iso
+    ]
 
+    db.query(query,values,function(err,results){
+      if(err) return res.send("Erro na base de dados")
+  
+      callback(results.rows[0])
+    })
+
+  },
+  find(id,callback) {
+    db.query(`SELECT * 
+    FROM  membros 
+    WHERE id = $1 `, [id],function(err,results){
+      if (err) return res.send('ERRO no Banco de Dados')
+      callback(results.rows[0])
+    })
   }
 }
