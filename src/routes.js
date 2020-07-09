24f8dffc00a9d269a56490894fb31540237ab937
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-09 16:29:49 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const routes  = express.Router()
 const membro = require('./app/controllers/membros')
+const valor = require('./app/controllers/valores')
 
 
 routes.get('/',function(req,res){
   return res.redirect('/home')
 })
 
+routes.get('/home',membro.home)
+
 routes.get('/create',membro.create)
 routes.post('/create',membro.post)
-routes.delete('/create',membro.delete)
-
-routes.get('/membros/:id',membro.show)
-routes.get('/membros/:id/edit',membro.edit)
-routes.put('/mebros/:id',membro.put)
 
 routes.get('/membros',membro.membros)
-routes.get('/home',membro.home)
+routes.get('/membros/:id',membro.show)
+routes.get('/membros/:id/edit',membro.edit)
+routes.put('/membros/:id',membro.put)
+routes.delete('/membro',membro.delete)
+
+routes.get('/membros/:id/addvalor',valor.addvalor)
+//routes.post('/membros/:id/addvalor',valor.postvalores)
+
+
+
 
 
 
